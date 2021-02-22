957d475f652419c7e037584b6958b04d6f8da3ef
CODA Designer <72040415+alan-dvloper@users.noreply.github.com>
2021-02-21 19:46:34 -0600

    Add files via upload

---
+++ javascript/alerta_share.js
@@ -0,0 +1,11 @@
+Swal.fire({
+   title: '<h3 class="titulo">Comparte nuestro diseño</h3>',
+   footer: '<p class="footer">Ingresa a tu red social favorita</p>',
+   html: '<a class="icon" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a><a class="icon" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a><a class="icon" href="https://twitter.com/"><i class="fab fa-twitter"></i></a><a class="icon" href="https://www.pinterest.com.mx/"><i class="fab fa-pinterest"></a>',
+   icon: 'info',
+   grow: 'fullscreen',
+   allowOutsideClick: false,
+   allowEscapeKey: false,
+   allowEnterKey: false,
+   showConfirmButton: false
+  })
